34a6b55e83af7bb8461bbfb7aca719f98910dbeb
juan1701711109 <juan.1701711109@ucaldas.edu.co>
2021-11-03 19:26:54 -0500

    Implementacion del CKEditor

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 require('./bootstrap');
+const ClassicEditor = require('@ckeditor/ckeditor5-build-classic');
+ClassicEditor.create(document.querySelector('#content_publication'))
+    .then(editor => {
+        console.log(editor);
+    })
+    .catch(error => console.log(error))
